df8cfd83d0bd593decbca0ca513b9911b171a571
mzsweeting <116032509+mzsweeting@users.noreply.github.com>
2024-02-24 05:50:43 -0500

    Updated h2

--- src/Weather.js
+++ src/Weather.js
@@ -9,5 +9,5 @@ export default function Weather(props) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
-    return <h2>Hello from Weather</h2>
+    return <h2>Hello Weather from Atlanta!</h2>
 }
